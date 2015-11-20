47326bbc49f8d51b6fe38890d344e75fc2c9642e
Mathieu Bernard <mathieu_bernard@laposte.net>
2015-11-20 14:02:11 +0100

    ignore phillips temp files, rm dummy spaces

--- database_creation/scripts/selcha2clean.sh
+++ database_creation/scripts/selcha2clean.sh
@@ -15,41 +15,41 @@ echo "Cleaning $SELFILE"
 #replacements to clean up punctuation, etc. -- usually ok regardless of the corpus
 
 iconv -f ISO-8859-1 "$RESFOLDER$SELFILE" |
-sed 's/^....:.//g' | 
-sed "s/\_/ /g" | 
+sed 's/^....:.//g' |
+sed "s/\_/ /g" |
 sed '/^0(.*) .$/d' |
 sed  's/.*$//g' |
-tr -d '\"' | 
-tr -d '\"' | 
-tr -d '\/' | 
-sed 's/\+/ /g' | 
-tr -d '\.' | 
-tr -d '\?' | 
-tr -d '!' | 
-tr -d ';' | 
-tr -d '\<' | 
-tr -d '\>' | 
-tr -d ','  | 
-tr -d '(' | 
-tr -d ')' | 
-tr -d ':'  | 
-sed 's/&[^ ]*//g' | 
-grep -v '\[- spa\]' | 
-sed 's/[^ ]*@sspa//g' | 
-sed 's/ \[.*\]//g' | 
-sed 's/xxx//g' | 
-sed 's/www//g' | 
-sed 's/XXX//g' | 
+tr -d '\"' |
+tr -d '\"' |
+tr -d '\/' |
+sed 's/\+/ /g' |
+tr -d '\.' |
+tr -d '\?' |
+tr -d '!' |
+tr -d ';' |
+tr -d '\<' |
+tr -d '\>' |
+tr -d ','  |
+tr -d '(' |
+tr -d ')' |
+tr -d ':'  |
+sed 's/&[^ ]*//g' |
+grep -v '\[- spa\]' |
+sed 's/[^ ]*@sspa//g' |
+sed 's/ \[.*\]//g' |
+sed 's/xxx//g' |
+sed 's/www//g' |
+sed 's/XXX//g' |
 sed 's/yyy//g' |
 sed 's/0*//g' |
-sed 's/@o//g' | 
-sed 's/@f//g' | 
-sed 's/@q//g' | 
-sed 's/@u//g' | 
-sed 's/@c//g' | 
-sed "s/\' / /g"  | 
-sed 's/  / /g' | 
-sed 's/ $//g' | 
+sed 's/@o//g' |
+sed 's/@f//g' |
+sed 's/@q//g' |
+sed 's/@u//g' |
+sed 's/@c//g' |
+sed "s/\' / /g"  |
+sed 's/  / /g' |
+sed 's/ $//g' |
 sed 's/^ //g' |
 sed 's/^[ ]*//g' |
 sed 's/ $//g' |
@@ -59,36 +59,33 @@ awk '{gsub("\"",""); print}' > tmp.tmp
 
 
 #********** A T T E N T I O N ***************#
-# check that the next set of replacements for unusual spellings is adapted to your purposes 
+# check that the next set of replacements for unusual spellings is adapted to your purposes
 #next set of replacements for unusual spellings - English
-	sed 's/allgone/all gone/g' tmp.tmp | 
-	 sed 's/[0-9]//g' | 
-	 sed 's/whaddaya/what do you/g' | 
-	sed 's/whadda/what do/g' | 
-	sed 's/haveto/have to/g' | 
-	sed 's/hasto/has to/g' | 
-	sed 's/outof/out of/g' | 
-	sed 's/lotsof/lots of/g' | 
-	sed 's/lotta/lots of/g' | 
-	sed 's/alotof/a lot of/g' | 
-	sed "s/wha\'s/what's/g" | 
-	sed "s/this\'s/this is/g" | 
-	sed 's/chya/ you/g' | 
-	 sed 's/tcha/t you/g' | 
-	sed 's/dya /d you /g' | 
-	sed 's/chyou/ you/g' | 
-	sed "s/dont you/don\'t you/g" | 
-	sed 's/wanta/wanna/g'  | 
-	sed "s/whats / what\'s /g" | 
-	sed "s/'re/ are/g" | 
-	sed "s/klenex/kleenex/g" | 
-	sed 's/yogourt/yogurt/g' | 
-	sed 's/weee*/wee/g' | 
-	sed 's/oooo*/oh/g' | 
-	sed 's/ oo / oh /g' | 
-	sed 's/ohh/oh/g' | 
-	sed "s/ im / I\'m /g" | 
+	sed 's/allgone/all gone/g' tmp.tmp |
+	 sed 's/[0-9]//g' |
+	 sed 's/whaddaya/what do you/g' |
+	sed 's/whadda/what do/g' |
+	sed 's/haveto/have to/g' |
+	sed 's/hasto/has to/g' |
+	sed 's/outof/out of/g' |
+	sed 's/lotsof/lots of/g' |
+	sed 's/lotta/lots of/g' |
+	sed 's/alotof/a lot of/g' |
+	sed "s/wha\'s/what's/g" |
+	sed "s/this\'s/this is/g" |
+	sed 's/chya/ you/g' |
+	 sed 's/tcha/t you/g' |
+	sed 's/dya /d you /g' |
+	sed 's/chyou/ you/g' |
+	sed "s/dont you/don\'t you/g" |
+	sed 's/wanta/wanna/g'  |
+	sed "s/whats / what\'s /g" |
+	sed "s/'re/ are/g" |
+	sed "s/klenex/kleenex/g" |
+	sed 's/yogourt/yogurt/g' |
+	sed 's/weee*/wee/g' |
+	sed 's/oooo*/oh/g' |
+	sed 's/ oo / oh /g' |
+	sed 's/ohh/oh/g' |
+	sed "s/ im / I\'m /g" |
 	iconv -t ISO-8859-1 > "$RESFOLDER$ORTHO"
-
-
-
