c89506b73287be34f50af898a8b6cd528f4cad60
Mathieu Bernard <mathieu_bernard@laposte.net>
2015-11-29 18:40:34 +0100

    Updated puddle algo for crosseval. Need to be clusterized.

--- algoComp/pipeline/puddle.sh
+++ algoComp/pipeline/puddle.sh
@@ -14,19 +14,34 @@ ALGO="puddle"
 cd ${ABSPATH}algos/PUDDLE
 
 # Remove word and syllable tags to create input:
-cat $ROOT-tags.txt |
-    sed 's/;esyll//g' |
+sed 's/;esyll//g' $ROOT-tags.txt |
     sed 's/;eword/ /g' |
-    sed 's/  *//g' > clean_test.txt
+    sed 's/  *//g' > $ROOT-$ALGO-input.txt
 
-# Actual algo running
-gawk -f segment.vowelconstraint.awk clean_test.txt > dirty_output.txt
+# create 5 folds for cross evaluation.  This creates 5 files
+# $ROOT-$ALGO-input-fold$N.txt and an index file
+# $ROOT-$ALGO-input-index.txt
+$ABSPATH/crossevaluation.py fold $ROOT-$ALGO-input.txt -n 5
 
-# Clean up the output file & store it in your desired location
-sed "s/.*://" dirty_output.txt  > $ROOT-${ALGO}-cfgold.txt
+for FOLD in $ROOT-$ALGO-input-fold*.txt
+do
+    echo Processing $FOLD
+    # Remove word and syllable tags
+    sed 's/;esyll//g' $FOLD |
+        sed 's/;eword/ /g' |
+        sed 's/  *//g' |
+        # Actual algo running
+        gawk -f segment.vowelconstraint.awk |
+        # Clean up the output & store it
+        sed 's/.*:\s//' |
+        sed 's/\s;aword//g' |
+        sed 's/\s*$//g' > ${FOLD/input/output}
+done
 
-# Local clean up
-#rm *.txt
+# unfold the results. Creates the file $ROOT-$ALGO-output-unfolded.txt
+$ABSPATH/crossevaluation.py unfold $ROOT-$ALGO-output-fold*.txt \
+                            --index $ROOT-$ALGO-input-index.txt
+mv $ROOT-$ALGO-output-unfolded.txt $ROOT-$ALGO-cfgold.txt
 
 # Do the evaluation
 cd ${ABSPATH}scripts
