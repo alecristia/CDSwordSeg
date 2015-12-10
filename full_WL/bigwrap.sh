7dd20c44cc8f2ca589e54d5315f78e41b4b4490e
Mathieu Bernard <mmathieubernardd@gmail.com>
2015-12-10 21:22:41 +0100

    Updated top level WL bigwrap, TODO.org recap all recent changes

--- full_WL/bigwrap.sh
+++ full_WL/bigwrap.sh
@@ -4,19 +4,20 @@
 # Mathieu Bernard
 
 # Create the trs folder and put Winnipeg trs files in it
-./0_gettrs.sh
+#./0_gettrs.sh
 
 # Turn the trs files into cha-like format
-./1_trstocha.sh
+./1_trs2cha.sh
 
 # Turn the cha-like files into a single clean file per type
-./2_onefilepercorpus.sh
+./2_cha2ortho.sh
 
 # Phonologize the ortholines files
-./3_phonologize.sh
+./3_ortho2phono.sh
 
 # Add length-matched versions of the CDS corpora
 ./4_special_step.sh
 
 # Analyze
-./5_analyze.sh
+# ./5_analyze.sh
+# ./6_collapse_results.sh
