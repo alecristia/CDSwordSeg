c59f082eb6db3e173f1668b25812e1a703a5bcbb
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-03-09 00:34:43 +0100

    updated childes recipe

--- recipes/childes/3_analyze.sh
+++ recipes/childes/3_analyze.sh
@@ -15,8 +15,10 @@ data_dir=${2:-./data}
 # the segmentation pipeline
 segmenter=${3:-../../algoComp/segment.py}
 
-# Run all algos on all speakers for all subcorpora in parallel (on the
-# cluster if available)
+# Because the whole corpus is too big (requires more than 10k jobs to
+# analyze it), this script analyzes only one sample (the first sample
+# not found in the result directory) and exit. This submit about 15
+# jobs to the cluster. You must rerun the script for each sample.
 for subcorpus in $(find . -maxdepth 2 -mindepth 2 -type d -exec basename {} \;)
 do
     # create the output dir if needed
@@ -24,12 +26,12 @@ do
 
     for speaker in $(find $data_dir/$subcorpus -mindepth 1 -type d -exec basename {} \;)
     do
-        echo "Clusterizing  $subcorpus $speaker"
-
-        input_dir=$data_dir/$subcorpus/$speaker
         result_dir=$output_dir/$subcorpus/$speaker
+        [ -d $result_dir ] && echo "found $subcorpus $speaker" && continue
+
+        echo "Clusterizing  $subcorpus $speaker"
+        input_dir=$data_dir/$subcorpus/$speaker
         version=$subcorpus.$speaker
-        #echo $input_dir $result_dir $version
 
         $segmenter --output-dir $result_dir \
                    --algorithms all \
@@ -37,9 +39,7 @@ do
                    --clusterize \
                    --jobs-basename $version \
                    --goldfile $input_dir/gold.txt \
-                   $input_dir/tags.txt || exit 1
-        exit
+                   $input_dir/tags.txt && exit 0 || exit 1
     done
-    exit
 done
 exit
