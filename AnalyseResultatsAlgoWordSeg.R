840ff36165415401846bcd8e18b4ae1871e68668
elarsen <elarsen@oberon.cm.cluster>
2016-11-16 11:25:43 +0100

    directory results created

--- AnalyseResultatsAlgoWordSeg.R
+++
@@ -1,9 +0,0 @@
-setwd("~/Documents/CDSwordSeg")
-results_cds_childes <- read.csv("~/Documents/CDSwordSeg/results_cds_childes.txt", sep="")
-results_bern<-subset(results_cds_childes ,results_cds_childes$corpus=="Bernstein")
-bern_dibs<-subset(results_bern,results_bern$algo=="dibs")
-bern_dibs
-dim(bern_dibs)
-hist(bern_dibs$token_f.score)
-mean(bern_dibs$token_f.score)
-mean(bern_dibs$boundary_f.score)
