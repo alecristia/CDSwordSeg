16cd4279e3e866d850944acc49efa5b10d471cc2
elarsen <elarsen@oberon.cm.cluster>
2016-11-21 13:46:06 +0100

    results brent dibs TPs puddle ngrams

--- algoComp/launch_algo.sh
+++ algoComp/launch_algo.sh
@@ -5,7 +5,7 @@ do
 for i in {0..9}
 do 
 #python segment.py --algorithms $algo -d res-sub-bern-ADS/sub$i -g ../recipes/bernstein/data_06_10/ADS/subphono/sub$i/sub_gold_bernstein_ADS_$i.txt ../recipes/bernstein/data_06_10/ADS/subphono/sub$i/sub_bernstein_ADS_$i.txt
-python segment.py --algorithms $algo -d res-sub-bern-CDS/sub$i -g ../recipes/bernstein/data_06_10/CDS/sub$i/gold.txt ../recipes/bernstein/data_06_10/CDS/sub$i/tags.txt
+python segment.py --algorithms $algo -d ../results/res-providence-CDS/sub$i -g ../recipes/childes/data/Providence/sub$i/gold.txt ../recipes/childes/data/Providence/sub$i/tags.txt
 echo sub $i done
 done
 echo algo $algo done
