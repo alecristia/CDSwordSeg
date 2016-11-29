484a437b173298a89f2a8b462838c05874097a6e
alvaro-itu <alvaro_miz@hotmail.com>
2016-11-29 14:51:02 +0100

    little bug fixes in the phonologization process and adaptation to CDS and ADS simultaneus process.

--- recipes/arglongitudinal/2_phonologize.sh
+++ recipes/arglongitudinal/2_phonologize.sh
@@ -11,99 +11,107 @@ LANGUAGE="aspanish" #right now, only options are qom, english and aspanish (arge
 
 PATH_TO_SCRIPTS="/home/lscpuser/Documents/CDSwordSeg/phonologization"	#path to the phonologization folder - E.g. PATH_TO_SCRIPTS="/home/xcao/cao/projects/ANR_Alex/CDSwordSeg/phonologization/"
 
-RES_FOLDER="/home/lscpuser/Documents/lscp-ciipme-gh/transcripciones/RES_FOLDER/CDS"	#this is where we will put the processed versions of the transcripts E.g. RES_FOLDER="/home/xcao/cao/projects/ANR_Alex/res_Childes_Eng-NA_cds/" - NOTICE THE / AT THE END OF THE NAME
+RES_FOLDER="/home/lscpuser/Documents/lscp-ciipme-gh/transcripciones/RES_FOLDER"	#this is where we will put the processed versions of the transcripts E.g. RES_FOLDER="/home/xcao/cao/projects/ANR_Alex/res_Childes_Eng-NA_cds/" - NOTICE THE / AT THE END OF THE NAME
 
 
-for ORTHO in ${RES_FOLDER}/2*ortholines.txt; do
-	KEYNAME=$(basename "$ORTHO" -ortholines.txt)
+for CORPUSFOLDER in $RES_FOLDER/*DS; do
+	cd $CORPUSFOLDER
+	for ORTHO in $CORPUSFOLDER/2*ortholines.txt; do
+		KEYNAME=$(basename "$ORTHO" -ortholines.txt)
 
 	#########
-	if [ "$LANGUAGE" = "qom" ]
-	   then
-	  echo "recognized $LANGUAGE"
-	  tr '[:upper:]' '[:lower:]' < "$ORTHO"  | # change uppercase letters to lowercase letters
-	  tr -d '^M' |
-	  sed 's/ch/C/g' |
-	  sed 's/sh/S/g' |
-	  sed 's/ñ/N/g' |
-	  tr "'" "Q"  |
-	  iconv -t ISO-8859-1 > intopearl.tmp
+		if [ "$LANGUAGE" = "qom" ]
+	   	   then
+	  	   echo "recognized $LANGUAGE"
+	  	   tr '[:upper:]' '[:lower:]' < "$ORTHO"  | # change uppercase letters to lowercase letters
+	  	   tr -d '^M' |
+	  	   sed 's/ch/C/g' |
+	  	   sed 's/sh/S/g' |
+	  	   sed 's/ñ/N/g' |
+	  	   tr "'" "Q"  |
+	  	   iconv -t ISO-8859-1 > intopearl.tmp
 
-	  echo "syllabify-corpus.pl"
-	  perl $PATH_TO_SCRIPTS/scripts/syllabify-corpus.pl qom intopearl.tmp outofperl.tmp $PATH_TO_SCRIPTS
+	  	   echo "syllabify-corpus.pl"
+	  	   perl $PATH_TO_SCRIPTS/scripts/syllabify-corpus.pl qom intopearl.tmp outofperl.tmp $PATH_TO_SCRIPTS
 
-	  echo "removing blank lines"
-	  sed '/^$/d' outofperl.tmp |
-	  sed '/^ $/d'  |
-	  sed 's/^\///' |
-	sed 's/ / \;eword /g' |
-	sed 's/\// \;esyll /g' > tmp.tmp
+	  	   echo "removing blank lines"
+	  	   sed '/^$/d' outofperl.tmp |
+	  	   sed '/^ $/d'  |
+	  	     sed 's/^\///' |
+		   sed 's/ / \;eword /g' |
+		   sed 's/\// \;esyll /g' > tmp.tmp
 
-	  mv tmp.tmp ${RES_FOLDER}/${KEYNAME}-tags.txt
+	  	   mv tmp.tmp ${RES_FOLDER}/${KEYNAME}-tags.txt
 
-echo "creating gold versions"
+		echo "creating gold versions"
 
-sed 's/;esyll//g'  < ${RES_FOLDER}/${KEYNAME}-tags.txt |
-    sed 's/ //g' |
-    sed 's/;eword/ /g' > ${RES_FOLDER}/${KEYNAME}-gold.txt
+		sed 's/;esyll//g'  < ${RES_FOLDER}/${KEYNAME}-tags.txt |
+    			sed 's/ //g' |
+    		sed 's/;eword/ /g' > ${RES_FOLDER}/${KEYNAME}-gold.txt
 
 
-	elif [ "$LANGUAGE" = "aspanish" ]
-	   then
-	  echo "recognized $LANGUAGE"
-    tr '[:upper:]' '[:lower:]' < "$ORTHO" | # change uppercase letters to lowercase letters
-	  tr -d '^M' |
-	  sed 's/ch/tS/g' | # substitute all ch by tS
-	  sed 's/v/b/g' |
-	  sed 's/z/s/g' |
-	  sed 's/ca/ka/g' |
-	  sed 's/co/ko/g' |
-	  sed 's/cu/ku/g' |
-	  sed 's/ce/se/g' |
-	  sed 's/ci/si/g' |
-	  sed 's/rr/R/g' | # substitute the spanish rr by 5
-	  sed 's/ r/ R/g' | # substitue the initial r for R
-	  sed 's/ya/Sa/g' | # substitute all y by S (argentinian, rioplatense)
-	  sed 's/ye/Se/g' |
-	  sed 's/yi/Si/g' |
-	  sed 's/yo/So/g' |
-	  sed 's/yu/Su/g' |
-	  sed 's/ay/ai/g' | # substitute all y glides by i.
-	  sed 's/oy/oi/g' |
-	  sed 's/uy/ui/g' |
-	  sed 's/ll/S/g' |
-	  sed 's/j/x/g' |
-	  sed 's/qu/k/g' |
-	  sed 's/h//g' | # removing h
-	  sed 's/ñ/N/g' |
-	  sed 's/á/a/g' |
-	  sed 's/é/e/g' |
-	  sed 's/í/i/g' |
-	  sed 's/ó/o/g' |
-	  sed 's/ú/u/g' |
-	  sed 's/ü/u/g' |
-	  iconv -t ISO-8859-1 > intoperl.tmp
+		elif [ "$LANGUAGE" = "aspanish" ]
+	   	     then
+	  	  echo "recognized $LANGUAGE"
+    		tr '[:upper:]' '[:lower:]' < "$ORTHO" | # change uppercase letters to lowercase letters
+	  	  tr -d '^M' |
+		  tr -d '\n' |
+	  	  sed 's/ch/tS/g' | # substitute all ch by tS
+	  	  sed 's/z/s/g' |
+                  sed 's/ñ/N/g' |
+                  sed 's/á/a/g' |
+                  sed 's/é/e/g' |
+                  sed 's/í/i/g' |
+                  sed 's/ó/o/g' |
+                  sed 's/ú/u/g' |
+                  sed 's/ü/u/g' |
+                  sed 's/cl/kl/g' |
+                  sed 's/cr/kr/g' |
+		  sed 's/cc/ks/g' |
+	  	  sed 's/ca/ka/g' |
+	  	  sed 's/co/ko/g' |
+	  	  sed 's/cu/ku/g' |
+	  	  sed 's/ce/se/g' |
+	  	  sed 's/ci/si/g' |
+	  	  sed 's/rr/R/g' | # substitute the spanish rr by 5
+	  	  sed 's/ r/ R/g' | # substitue the initial r for R
+		  sed 's/^r/^R/g' |
+	  	  sed 's/ya/Sa/g' | # substitute all y by S (argentinian, rioplatense)
+	  	  sed 's/ye/Se/g' |
+	  	  sed 's/yi/Si/g' |
+	  	  sed 's/yo/So/g' |
+	  	  sed 's/yu/Su/g' |
+	  	  sed 's/ay/ai/g' | # substitute all y glides by i.
+		  sed 's/ey/ei/g' |
+	  	  sed 's/oy/oi/g' |
+	  	  sed 's/uy/ui/g' |
+		  sed 's/ y/ i/g' |
+	  	  sed 's/ll/S/g' |
+	  	  sed 's/j/x/g' |
+	  	  sed 's/qu/k/g' |
+	  	  sed 's/h//g' | # removing h
+	  	  iconv -t ISO-8859-1 > intoperl.tmp
 
-	  echo "syllabify-corpus.pl"
-	  perl $PATH_TO_SCRIPTS/scripts/syllabify-corpus.pl aspanish intoperl.tmp outofperl.tmp $PATH_TO_SCRIPTS
+	  	  echo "syllabify-corpus.pl"
+	  	  perl $PATH_TO_SCRIPTS/scripts/syllabify-corpus.pl aspanish intoperl.tmp outofperl.tmp $PATH_TO_SCRIPTS
 
-	  echo "removing blank lines"
-	  sed '/^$/d' < outofperl.tmp  |
-	  sed '/^ $/d'  |
-	  sed 's/^\///' |
-	  sed -n '$p' |
-	  sed 's/ / \;eword /g' |
-	  sed -e 's/\(.\)/\1 /g' |
-	  sed 's/\ ; e w o r d/\;eword/g' |
-	  sed 's/\//\;esyll /g' > tmp.tmp
+	  	  echo "removing blank lines"
+	  	  sed '/^$/d' < outofperl.tmp  |
+	  	  sed '/^ $/d'  |
+	  	  sed 's/^\///' |
+	  	 # sed -n '$p' |		#takes only the last line and errases the previous repetitions.
+	  	  sed 's/ / \;eword /g' |
+	  	  sed -e 's/\(.\)/\1 /g' |
+	  	  sed 's/\ ; e w o r d/\;eword/g' |
+	  	  sed 's/\//\;esyll /g' > tmp.tmp
 
-	  mv tmp.tmp ${RES_FOLDER}/${KEYNAME}-tags.txt
+	  	  mv tmp.tmp $CORPUSFOLDER/${KEYNAME}-tags.txt
 
-echo "creating gold versions"
+	echo "creating gold versions"
 
-sed 's/;esyll/ /g'  < ${RES_FOLDER}/${KEYNAME}-tags.txt |
-    sed 's/ //g' |
-    sed 's/;eword/ /g' > ${RES_FOLDER}/${KEYNAME}-gold.txt
+	sed 's/;esyll/ /g'  < $CORPUSFOLDER/${KEYNAME}-tags.txt |
+    	    sed 's/ //g' |
+    	    sed 's/;eword/ /g' > $CORPUSFOLDER/${KEYNAME}-gold.txt
 
 
         elif [ "$LANGUAGE" = "english" ]
@@ -117,6 +125,6 @@ sed 's/;esyll/ /g'  < ${RES_FOLDER}/${KEYNAME}-tags.txt |
 		echo "Adapt the script to a new language"
 		echo "I don't know $LANGUAGE"
 	fi
+	done
 done
-
 echo "end"
