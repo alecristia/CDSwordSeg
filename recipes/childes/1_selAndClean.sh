ab29798b6ad7a280e0c01e6bb9f8cf837143f5d8
Alejandrina Cristia <alecristia@gmail.com>
2017-01-02 11:40:59 +0100

    new exp. 2

--- recipes/childes/1_selAndClean.sh
+++ recipes/childes/1_selAndClean.sh
@@ -16,19 +16,12 @@ mkdir -p $PROCESSED_FOLDER	#create folder that will contain all output files
 
 cat $INPUT_FILE | while read line; do
   OUT_GROUP=`echo $line | awk -F "," '{ print $1}'`
-  CHAFILE=`echo $line | awk -F "," '{ print $4, "/", $5}'`
+  CHAFILE=`echo $line | awk -F "," '{ print $2}'`
+  SELFILE=`echo $line | awk -F "," '{ print $2}'`
 
+echo "finding out who's a speaker in $INPUT_FOLDER$CHAFILE"
 
-
-done 
-
-for f in ${INPUT_CORPUS}/*.cha; do	#loop through all cha files
-
-#ATTENTION!!! SELECTION OF ONLY A SUBSET OF FILES!!!
-
-echo "finding out who's a speaker in $f"
-
-	    IncludedParts=`tr '\015' '\n' < $f | #for each file
+	    IncludedParts=`tr '\015' '\n' < $INPUT_FOLDER$CHAFILE | #for each file
 		grep "@ID" |      #take only @ID lines of the file
 		awk -F "|" '{ print $3, $8 }' | #let through only 3-letter code and role
         grep -v 'Child\|CHI\|Sister\|Brother\|Cousin\|Boy\|Girl\|Unidentified\|Sibling\|Target\|Non_Hum\|Play' | #remove all the children and non-human participants to leave only adults
@@ -38,10 +31,15 @@ echo "finding out who's a speaker in $f"
 		sed "s/%/\\\\\|*/g" | #add a pipe between every two
 		sed "s/\\\\\|.$//" ` #remove the pipe* next to the end of line & close the text call
 
+		SELFILE=$(basename "$f" .cha)"-includedlines.txt"
+		./${ROOT}database_creation/scripts/cha2sel_withinputParticipants.sh $INPUT_FOLDER$CHAFILE $SELFILE $PROCESSED_FOLDER$OUT_GROUP $IncludedParts
+
+
+done 
+
+
 		cd $PATH_TO_SCRIPTS	#move to folder with the 2 scripts and run them with the correct parameters
 
-		SELFILE=$(basename "$f" .cha)"-includedlines.txt"
-		bash ./scripts/cha2sel_withinputParticipants.sh $f $SELFILE $PROCESSED_FOLDER/AS/ $IncludedParts
 
 		mkdir -p $PROCESSED_FOLDER/CDS	#creates folder that will contain all only CDS included files
               	grep '\[+ CHI\]' < $PROCESSED_FOLDER/AS/$SELFILE > $PROCESSED_FOLDER/CDS/$SELFILE  # selects only CDSinput lines.
