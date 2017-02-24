27089c08e7c6d658bd17350f59fb56f7353c7eda
elinlarsen <elin_larsen1@hotmail.fr>
2017-02-24 14:10:11 +0100

    AG pipeline modified for unit representation

--- algoComp/pipeline/algo_pipeline.sh
+++ algoComp/pipeline/algo_pipeline.sh
@@ -8,6 +8,7 @@ RESFOLDER=$2
 ALGO=$3
 UNIT=$4
 WINDOW=$5
+SETUP=$6 # only for 'AGu' or 'AG3cf'
 
 #UNIT is either 'syllable' or 'phoneme'
 
@@ -17,7 +18,7 @@ res=$RESFOLDER/$ALGO/$UNIT
 echo cleaning tags to get the right unit representation :  $UNIT
 $python ../InputPipeline.py -u $UNIT -r $res -p $ABSPATH/tags.txt
 
-if [ $ALGO=="TPs" ]
+if [ $ALGO = "TPs" ]
 # TP need UB at the end of utterance instead of \n
 then
 #sed 's/\n/UB /g' $res/input.txt > $res/input_bis.txt
@@ -29,7 +30,7 @@ sed 's/  / /g' $res/input.txt |
 $python ../algos/TPs/TPsegmentation.py $res/boundaries_marked.txt > $res/cfgold.txt
 
 # actual algo running
-elif [ $ALGO=="ngrams" ]
+elif [ $ALGO = "ngrams" ]
 then
     BIN=$../algos/ngrams/mkngram.sh
     $BIN --syll $res/input.txt > $res/freq-all.txt
@@ -39,25 +40,25 @@ then
 # Note: n-grams doesn't have a full eval, like the others, because
 # it's not a SEGMENTATION algorithm
 
-elif [ $ALGO=="puddle" ]
+elif [ $ALGO == "puddle" ]
 then
 $python ../algos/PUDDLE/puddle_new.py -r $res -o $res/cfgold.txt -w $WINDOW
 
 
-elif [ $ALGO="dibs" ]
+elif [ $ALGO = "dibs" ]
 # DIBS requires a bit of corpus to calculate some statistics. We'll
 # use 200 lines from the version with the word boundaries to this end
 # (we remove syllable boundaries, which are not needed):
 then
 echo training $ALGO
-head -200 $res/tags.txt | sed 's/;esyll//g' > $res/clean_train.txt
+head -200 $ABSPATH/tags.txt | sed 's/;esyll//g' > $res/clean_train.txt
 
 echo Running $ALGO...
 DIBS=../algos/DiBS/apply-dibs.py
 
 # Actual algo running
 $DIBS $res/clean_train.txt $res/input.txt \
-$RESFOLDER/dirty_output.txt $RESFOLDER/diphones_output.txt
+      $res/dirty_output.txt $res/diphones_output.txt
 
 # Clean up the output file & store it in your desired location
 sed "s/.*$(printf '\t')//" $res/dirty_output.txt |
@@ -67,10 +68,8 @@ sed 's/ //g' $res/output.txt |
 sed 's/;aword/ /g' > $res/cfgold.txt
 
 
-elif [ $ALGO=="AG" ]
+elif [ $ALGO = "AGu" ]
 then
-GRAMMARFILE=$../algos/AG/do_AG_english.sh # to add a variable indicating the unit representation : syllable or phoneme
-
 # Remove spaces within words and syllable boundaries, and replace word
 # tags with spaces to create gold
 sed 's/;esyll//g' $ABSPATH/tags.txt |
@@ -78,12 +77,17 @@ sed 's/;eword/ /g' |
 sed '/^$/d' |
 tr -d ' ' > $res/input.gold
 
-# Remove word and syllable tags to create input
-sed '/^$/d' $res/input.txt |
-sed 's/  */ /g' > $RESFOLDER/input.ylt
+#copie the tag file in the result folder
+cp $ABSPATH/tags.txt $res/tags.txt
+cp $ABSPATH/gold.txt $res/gold.txt
+
+# input in ylt 
+cp $res/input.txt $res/input.ylt
 
 # actual algo running
-$GRAMMARFILE $res $ALGO $DEBUG $UNIT|| exit 1
+echo running $ALGO
+GRAMMARFILE=../algos/AG/do_AG_english.sh
+$GRAMMARFILE $res $ALGO $UNIT $SETUP|| exit 1
 
 fi
 
