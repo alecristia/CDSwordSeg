85117c4a77ff84f6aeb0277169c193a8c6c11e71
elinlarsen <elin_larsen1@hotmail.fr>
2017-02-24 10:01:12 +0100

    Pipeline for unit of representation phoneme or syllable

---
+++ algoComp/pipeline/algo_pipeline.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+# Script for launching n-grams, the step 4 of segment_one_corpus.sh
+# Author: Mathieu Bernard <mmathieubernardd@gmail.com>
+
+ABSPATH=$1
+RESFOLDER=$2
+ALGO=$3
+UNIT=$4
+WINDOW=$5
+
+#UNIT is either 'syllable' or 'phoneme'
+
+res=$RESFOLDER/$ALGO/$UNIT
+
+# Remove word boundaries to create input:
+echo cleaning tags to get the right unit representation :  $UNIT
+$python ../InputPipeline.py -u $UNIT -r $res -p $ABSPATH/tags.txt
+
+if [ $ALGO=="TPs" ]
+# TP need UB at the end of utterance instead of \n
+then
+#sed 's/\n/UB /g' $res/input.txt > $res/input_bis.txt
+sed 's/  / /g' $res/input.txt |
+    sed 's/ $//g' |
+    tr '\n' '?'   |
+    sed 's/?/ UB /g' > $res/boundaries_marked.txt
+#echo '\n' > $res/boundaries_marked.txt
+$python ../algos/TPs/TPsegmentation.py $res/boundaries_marked.txt > $res/cfgold.txt
+
+# actual algo running
+elif [ $ALGO=="ngrams" ]
+then
+    BIN=$../algos/ngrams/mkngram.sh
+    $BIN --syll $res/input.txt > $res/freq-all.txt
+    head -n 10000 $res/freq-all.txt > $res/freq-top.txt
+
+
+# Note: n-grams doesn't have a full eval, like the others, because
+# it's not a SEGMENTATION algorithm
+
+elif [ $ALGO=="puddle" ]
+then
+$python ../algos/PUDDLE/puddle_new.py -r $res -o $res/cfgold.txt -w $WINDOW
+
+
+elif [ $ALGO="dibs" ]
+# DIBS requires a bit of corpus to calculate some statistics. We'll
+# use 200 lines from the version with the word boundaries to this end
+# (we remove syllable boundaries, which are not needed):
+then
+echo training $ALGO
+head -200 $res/tags.txt | sed 's/;esyll//g' > $res/clean_train.txt
+
+echo Running $ALGO...
+DIBS=../algos/DiBS/apply-dibs.py
+
+# Actual algo running
+$DIBS $res/clean_train.txt $res/input.txt \
+$RESFOLDER/dirty_output.txt $RESFOLDER/diphones_output.txt
+
+# Clean up the output file & store it in your desired location
+sed "s/.*$(printf '\t')//" $res/dirty_output.txt |
+sed 's/;eword/;aword/g' > $res/output.txt
+
+sed 's/ //g' $res/output.txt |
+sed 's/;aword/ /g' > $res/cfgold.txt
+
+
+elif [ $ALGO=="AG" ]
+then
+GRAMMARFILE=$../algos/AG/do_AG_english.sh # to add a variable indicating the unit representation : syllable or phoneme
+
+# Remove spaces within words and syllable boundaries, and replace word
+# tags with spaces to create gold
+sed 's/;esyll//g' $ABSPATH/tags.txt |
+sed 's/;eword/ /g' |
+sed '/^$/d' |
+tr -d ' ' > $res/input.gold
+
+# Remove word and syllable tags to create input
+sed '/^$/d' $res/input.txt |
+sed 's/  */ /g' > $RESFOLDER/input.ylt
+
+# actual algo running
+$GRAMMARFILE $res $ALGO $DEBUG $UNIT|| exit 1
+
+fi
+
+echo done with $ALGO
+
+
+# Do the evaluation
+####STEP 1: Evaluate against the gold
+$python ../scripts/evalGold.py -g $ABSPATH/gold.txt < $res/cfgold.txt \
+> $res/cfgold-res.txt
+
+####STEP 2: Extract top 10k frequency items
+tr ' ' '\n' < $res/cfgold.txt |
+sort | uniq -c | awk '{print $1" "$2}' | sort -n -r |
+head -n 10000 > $res/freq-top.txt
+
+
