b3687f432fe02550fd685c9915fba7483012b95c
elinlarsen <elin_larsen1@hotmail.fr>
2017-02-26 14:57:48 +0100

    AG pipeline modified for unit representation - working

--- algoComp/pipeline/algo_pipeline.sh
+++ algoComp/pipeline/algo_pipeline.sh
@@ -40,9 +40,56 @@ then
 # Note: n-grams doesn't have a full eval, like the others, because
 # it's not a SEGMENTATION algorithm
 
+elif [ $ALGO == "puddle_py" ]
+then
+
+NFOLDS=5
+#echo Creating $NFOLDS folds for cross evaluation
+$python ../crossevaluation.py fold $res/input.txt --nfolds $NFOLDS
+
+# TODO parallelize
+for FOLD in $res/input-fold*.txt
+do
+    N=`basename $FOLD | sed 's/.*fold//' | sed 's/\.txt//'`
+    echo Processing fold $N
+
+# Actual algo running
+$python ../algos/PUDDLE/puddle_new.py -i $FOLD -r $res -o ${FOLD/input/output} -w $WINDOW
+
+done
+
+echo Unfolding to cfgold.txt
+$python ../crossevaluation.py unfold $res/output-fold*.txt --index $res/input-index.txt --output $res/cfgold.txt
+
+
+
 elif [ $ALGO == "puddle" ]
 then
-$python ../algos/PUDDLE/puddle_new.py -r $res -o $res/cfgold.txt -w $WINDOW
+CROSSEVAL=$../crossevaluation.py
+PUDDLE="gawk -f $ABSPATH/algos/PUDDLE/segment.vowelconstraint.awk"
+NFOLDS=5
+echo Creating $NFOLDS folds for cross evaluation
+$CROSSEVAL fold $res/input.txt --nfolds $NFOLDS
+
+# TODO parallelize
+for FOLD in $res/input-fold*.txt
+do
+N=`basename $FOLD | sed 's/.*fold//' | sed 's/\.txt//'`
+echo Processing fold $N
+
+# Actual algo running
+$PUDDLE |
+# Clean up the output & store it
+sed 's/.*:\s//' |
+sed 's/\s;aword//g' |
+sed 's/\s*$//g' > ${FOLD/input/output}
+done
+
+echo Unfolding to cfgold.txt
+$CROSSEVAL unfold $res/output-fold*.txt \
+--index $res/input-index.txt \
+--output $res/cfgold.txt
+
 
 
 elif [ $ALGO = "dibs" ]
@@ -51,7 +98,17 @@ elif [ $ALGO = "dibs" ]
 # (we remove syllable boundaries, which are not needed):
 then
 echo training $ALGO
-head -200 $ABSPATH/tags.txt | sed 's/;esyll//g' > $res/clean_train.txt
+case $UNIT in
+    "phoneme")
+        head -200 $ABSPATH/tags.txt | sed 's/;esyll//g' > $res/clean_train.txt
+    ;;
+    "syllable")
+        head -200 $ABSPATH/tags.txt | sed 's/ //g' | sed 's/;esyll//g' > $res/clean_train.txt
+    ;;
+*)
+    echo something went wrong during training
+    exit 1
+esac
 
 echo Running $ALGO...
 DIBS=../algos/DiBS/apply-dibs.py
@@ -95,6 +152,7 @@ echo done with $ALGO
 
 
 # Do the evaluation
+echo evaluating $ALGO using $UNIT as input unit
 ####STEP 1: Evaluate against the gold
 $python ../scripts/evalGold.py -g $ABSPATH/gold.txt < $res/cfgold.txt \
 > $res/cfgold-res.txt
@@ -103,5 +161,6 @@ $python ../scripts/evalGold.py -g $ABSPATH/gold.txt < $res/cfgold.txt \
 tr ' ' '\n' < $res/cfgold.txt |
 sort | uniq -c | awk '{print $1" "$2}' | sort -n -r |
 head -n 10000 > $res/freq-top.txt
+echo evaluation done
 
 
