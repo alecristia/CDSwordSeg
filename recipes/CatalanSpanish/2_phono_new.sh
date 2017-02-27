4a6b3e7bcfca42272ebff90e8853e1a4aba75795
laiafr <laia.fibla.reixachs@gmail.com>
2017-02-27 17:31:30 +0100

    new phono

--- recipes/CatalanSpanish/2_phono_new.sh
+++ recipes/CatalanSpanish/2_phono_new.sh
@@ -6,51 +6,40 @@
 
 #########VARIABLES
 #Variables to modify
-LANGUAGE="cspanish" #language options:  cspanish (castillan spanish), catalan  -- NOTICE, IN SMALL CAPS
+LANGUAGE="catalan" #language options:  cspanish (castillan spanish), catalan  -- NOTICE, IN SMALL CAPS
 
 
 PATH_TO_SCRIPTS="/fhgfs/bootphon/scratch/lfibla/CDSwordSeg/phonologization"
 #path to the phonologization folder - E.g. PATH_TO_SCRIPTS="/home/xcao/cao/projects/ANR_Alex/CDSwordSeg/phonologization/"
 
-RES_FOLDER="/fhgfs/bootphon/scratch/lfibla/SegCatSpa/RES_corpus_spa/"
+RES_FOLDER="/fhgfs/bootphon/scratch/lfibla/SegCatSpa/RES_corpus_cat/"
 #this is where we will put the processed versions of the transcripts E.g. RES_FOLDER="/home/xcao/cao/projects/ANR_Alex/res_Childes_Eng-NA_cds/"
 # NOTICE THE / AT THE END OF THE NAME
 
+# espeak
+# elif [ "$LANGUAGE" = "catalan" ]
+#           then
+#          echo "recognized $LANGUAGE"
+#
+#          echo "using espeak"
+#          /fhgfs/bootphon/scratch/lfibla/phonemizer -l ca $ORTHO -o ${KEYNAME}-tags.txt
 
 for ORTHO in ${RES_FOLDER}*ortholines.txt; do
 	KEYNAME=$(basename "$ORTHO" -ortholines.txt)
 
 	#########
-	if [ "$LANGUAGE" = "cspanish" ]
+	if [ "$LANGUAGE" = "catalan" ]
 	   then
 	  echo "recognized $LANGUAGE"
-tr '[:upper:]' '[:lower:]'  < "$ORTHO"  | #Spanish files have different encoding
-	  sed 's/ch/C/g' | # substitute all ch by tS
-	  sed 's/v/b/g' |
-	  sed 's/z/O/g' |
-		sed 's/ce/Oe/g' |
-		sed 's/ci/Oi/g' |
-	  sed 's/c/k/g' |
-	  sed 's/rr/R/g' | # substitute the spanish rr by 5
-	  sed 's/ r/ R/g' | # substitue the initial r for R
-	  sed 's/^r/R/g' | # substitue the initial r for R
-	  sed 's/ll/L/g' | # very mixed in spain choose between L and y
-	  sed 's/j/x/g' |
-	  sed 's/qu/k/g' |
-	  sed 's/h//g' | # removing h
-	  sed 's/ñ/N/g' |
-	  sed 's/á/a/g' |
-	  sed 's/é/e/g' |
-	  sed 's/í/i/g' |
-	  sed 's/ó/o/g' |
-	  sed 's/ú/u/g' |
-	  sed 's/ü/u/g'  > intoperl.tmp
+
+		echo "using espeak"
+		phonemize -l ca $ORTHO -o intoperl.tmp
 
 	  echo "syllabify-corpus.pl"
 	  perl $PATH_TO_SCRIPTS/scripts/syllabify-corpus.pl aspanish intoperl.tmp outofperl.tmp $PATH_TO_SCRIPTS
 
 
-	elif [ "$LANGUAGE" = "catalan" ]
+elif [ "$LANGUAGE" = "cspanish" ]
 		 then
 		echo "recognized $LANGUAGE"
 	tr '[:upper:]' '[:lower:]'  < "$ORTHO"  | #Spanish files have different encoding
@@ -85,7 +74,7 @@ tr '[:upper:]' '[:lower:]'  < "$ORTHO"  | #Spanish files have different encoding
 		perl $PATH_TO_SCRIPTS/scripts/syllabify-corpus.pl aspanish intoperl.tmp outofperl.tmp $PATH_TO_SCRIPTS
 
 	fi
-
+"""
 		echo "removing blank lines"
 		sed '/^$/d' outofperl.tmp |
 		sed '/^ $/d'  |
@@ -97,7 +86,7 @@ tr '[:upper:]' '[:lower:]'  < "$ORTHO"  | #Spanish files have different encoding
 	sed 's/ ; e w o r d/ ;eword /g' |
 	sed 's/\// ;esyll /g'|
 	tr -s ' ' > tmp.tmp
-
+"""
 		mv tmp.tmp ${RES_FOLDER}/${KEYNAME}-tags.txt
 
 	echo "creating gold versions"
