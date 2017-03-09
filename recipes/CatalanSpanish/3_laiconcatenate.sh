6a55685c51df69f31413b775295a6b1e5ea0575b
laiafr <laia.fibla.reixachs@gmail.com>
2017-03-09 19:34:21 +0100

    conc

--- recipes/CatalanSpanish/3_laiconcatenate.sh
+++ recipes/CatalanSpanish/3_laiconcatenate.sh
@@ -8,10 +8,12 @@ echo $input $output
 for s in $input/*gold.txt
 do
 thistagfile=$(basename "$s" -gold.txt)
+#thistagfile=$(basename "$s")
         max=`wc -l $s | grep -v "total" | awk '{print $1}'`
         n=$(( ($max / 100)*100 ))
-
-        head -$n $s > ${s}-cutlines.txt
+echo everything ok
+      #  head -$n $s > ${s}-cutlines.txt
+        head -$n $s > ${thistagfile}-cutlines.txt
         head -$n ${input}/${thistagfile}-tags.txt > ${input}/${thistagfile}-cutlines.txt
 echo multiples of 100
 done
@@ -20,24 +22,22 @@ max=`wc -l $input/*cutlines.txt | grep -v "total" | awk '{print $1}' | sort -nr 
 
 for length in 2 100
 do
-	mkdir -p ${output}/$length/
-	add=$(( $length - 1 ))
+  rm -r ${output}/$length/
+  mkdir -p ${output}/$length/
+  add=$(( $length - 1 ))
 
-	i=1
-	while [ $i -lt $max ]
-	do
-
-#echo in while $i
-                j=$(( $i + $add ))
-								for thisfile in $input/*-cutlines.txt;
-								do
-#echo in for $thisfile
-                     	thistagfile=$(basename "$thisfile" -gold.txt)
-											sed -n $i,${j}p $thisfile >> ${output}/$length/gold.txt
-											sed -n $i,${j}p $input/${thistagfile}-cutlines.txt >> ${output}/$length/tags.txt
-                done
-        i=$(($i + $length ))
-        done
+  i=1
+  while [ $i -lt $max ]
+  do
+    j=$(( $i + $add ))
+    for thisfile in $input/*cutlines.txt
+    do
+      thistagfile=$(basename "$thisfile" -cutlines.txt)
+      sed -n $i,${j}p $thisfile >> ${output}/$length/gold.txt
+      sed -n $i,${j}p $input/${thistagfile}-cutlines.txt >> ${output}/$length/tags.txt
+    done
+  i=$(($i + $length ))
+  done
 done
 
 echo "done mixing lines for gold and tags"
