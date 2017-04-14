7107d9fdd6a7cbcc51cce114185682f8bf7f52a8
Alejandrina Cristia <alecristia@gmail.com>
2017-04-14 13:06:05 +0200

    separating extraction

--- recipes/acqDiv/1.sel_clean.r
+++ recipes/acqDiv/1.sel_clean.r
@@ -1,6 +1,9 @@
-acq<-load("/Users/bootphonproject/Desktop/segmentation/acqdiv_corpus_2016-09-22_ctn_jap.rda")
+acq<-load("../../../acqdiv_corpus_2016-09-22_ctn_jap.rda")
 utterance_data<-data.frame(utterances)
 clean_corpus<-utterance_data[ , c('language', 'utterance', 'speaker_label')]
-language_data<-clean_corpus[clean_corpus$language=="Japanese" & clean_corpus$speaker_label!="CHI", ]
-language_data_noCHI<-language_data[ ,2]
-write.table(language_data_noCHI, file="/Users/bootphonproject/Desktop/segmentation/results/japanese/clean_corpus.txt", row.names=F,col.names=F,quote=F)
+
+language_data<-clean_corpus[clean_corpus$speaker_label!="CHI",]
+
+write.table(language_data[language_data$language=="Japanese",2], file="../../../japanese_surface.txt", row.names=F,col.names=F,quote=F)
+write.table(language_data[language_data$language=="Chintang",2], file="../../../chintang_surface.txt", row.names=F,col.names=F,quote=F)
+
