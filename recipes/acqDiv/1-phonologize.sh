f78b2f334e4c0d23176c4d7397fcea5a8fd446e0
Alejandrina Cristia <alecristia@gmail.com>
2017-04-18 12:05:17 +0200

    minor corrections

---
+++ recipes/acqDiv/1-phonologize.sh
@@ -0,0 +1,153 @@
+#!/bin/sh
+# Wrapper to take a single cleaned up transcript and phonologize it
+# Alex Cristia alecristia@gmail.com 2015-10-26
+# Modified by Laia Fibla laia.fibla.reixachs@gmail.com 2016-09-28 adapted to arg spanish
+# Modified by Georgia Loukatou georgialoukatou@gmail.com 2017-04-02 adapted to chintang, japanese 
+
+
+#########VARIABLES
+LANGUAGE=$1 #language options:  japanese, chintang 
+PATH_TO_SCRIPTS=$2 # E.g. PATH_TO_SCRIPTS="CDSwordSeg/phonologization/"
+RES_FOLDER=$3 #this is where we find the acqdiv corpus in preprocessed and postprocessed forms/"
+####
+
+for ORTHO in ${RES_FOLDER}/data/${LANGUAGE}/*surface.txt; do
+	KEYNAME=$(basename "$ORTHO" .txt)
+
+#echo "$ORTHO"
+#echo "$LANGUAGE"
+#echo "$PATH_TO_SCRIPTS"
+#echo "$RES_FOLDER"
+
+	#########
+	if [ "$LANGUAGE" = "japanese" ]; then
+	  echo "recognized $LANGUAGE"
+tr '[:upper:]' '[:lower:]'  < "$ORTHO"  | 
+	  sed 's/ $//g' | #
+	  sed 's/^$//g' | #
+	  sed 's/ch/C/g' | #
+	  sed 's/sh/5/g' |
+	  sed 's/tt/T/g' |
+	  sed 's/kk/K/g' |
+	  sed 's/gg/G/g' |
+	  sed 's/ss/S/g' |
+	  sed 's/ʃ/J/g' |
+	  sed 's/ŋ/H/g' |
+	  sed 's/sy/W/g' |
+	  sed 's/zy/Q/g' |
+	  sed 's/ty/D/g' |
+	  sed 's/ɽ/R/g' |
+	  sed 's/ʒ/3/g' |
+	  sed 's/え/X/g' |
+	  sed 's/θ/V/g' |
+	  sed 's/pp/P/g' |
+	  sed 's/aa/A/g' |
+	  sed 's/ii/I/g' |
+	  sed 's/ee/E/g' |
+	  sed 's/oo/O/g' |
+	  sed 's/uu/U/g' |
+	  sed 's/ai/9/g' |
+	  sed 's/oi/4/g' |
+	  sed 's/au/8/g' |
+	  sed 's/ɘ/1/g' |
+	  sed 's/ə/2/g' > ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-intoperl.tmp
+
+
+	elif [ "$LANGUAGE" = "chintang" ]
+		 then
+		echo "recognized $LANGUAGE"
+	tr '[:upper:]' '[:lower:]'  < "$ORTHO"  | 
+		sed 's/ɨ/1/g' |
+		sed 's/ei/2/g' |
+		sed 's/ai/3/g' |
+		sed 's/oi/4/g' |
+		sed 's/ui/5/g' |
+		sed 's/au/6/g' |
+		sed 's/1i/7/g' |
+		sed 's/[àãâā]/A/g' |
+		sed 's/[ā]/A/g' |
+		sed 's/[ũùûù]/U/g' |
+		sed 's/[ôò]/O/g' |
+		sed 's/[èẽ]/E/g' |
+		sed 's/[ĩīĩ]/I/g' |
+		sed 's/jh//g' |
+		sed 's/kk/K/g' |
+		sed 's/tt/T/g' | # substitute all ch by tS	
+		sed 's/cc/C/g' |
+		sed 's/bb/B/g' |
+		sed 's/ss/S/g' |
+		sed 's/nn/N/g' |
+		sed 's/ñ/N/g' |
+		sed 's/mm/M/g' |
+		sed 's/jj/J/g' |
+		sed 's/lh/L/g' | # substitute the spanish rr by 5
+		sed 's/gh/G/g' | # substitue the initial r for R
+		sed 's/pp/P/g' | # substitue the initial r for R
+		sed 's/dh/D/g' |
+		sed 's/ḍ/D/g' |
+		sed 's/ch/Y/g' |
+		sed 's/jh/Ζ/g' |
+		sed 's/bh/V/g' | # removing h
+		sed 's/kh/Q/g' |
+		sed 's/th/X/g' |
+		sed 's/ʔ/q/g' |
+		sed 's/ṽ/w/g' |
+		sed 's/ŋ/H/g' |
+		sed 's/�/W/g' |
+		sed 's/m̄/m/' |
+		sed 's//W/' |
+		sed 's/Ḧ/H/' |
+		sed 's/Ë/E/' |
+		sed 's/ɲ/N/' |
+		sed 's/hAA̴/hAA/' |
+		sed 's/¨//'  |
+		sed 's/Œ ñ/Z/' |
+		sed 's/Œ £/Z/' |
+		sed 's/‡ • §//' |
+		sed 's/̵//'  |
+		sed 's/̪//'  |
+		sed 's/ǃ//'  |
+		sed 's/~//'  |
+		sed 's/ʌ//'  |
+		sed 's/˜//'  |
+		sed 's/।//'  |
+		sed 's/̴̴//'  |
+		sed 's/"//'  |
+		sed 's/lUɡE//' |
+		sed 's/IɡIMA//' |
+		sed 's/ph/F/g' > ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-intoperl.tmp
+
+	fi
+
+	  echo "syllabify-corpus.pl"
+	  perl $PATH_TO_SCRIPTS/scripts/new-syllabify-corpus.pl $LANGUAGE ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-intoperl.tmp ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-outofperl.tmp $PATH_TO_SCRIPTS
+
+
+		echo "removing blank lines"
+		LANG=C LC_CTYPE=C LC_ALL=C
+		sed '/^$/d' ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-outofperl.tmp |
+		sed '/^ $/d'  |
+		sed '/^[ ]*$/d'  |
+		sed 's/^ //'  |
+		sed 's/\n//' |
+		sed 's/^\///'  | #there aren't really any of these, this is just a cautionary measure
+	sed 's/ / ;eword /g' |
+		sed -e 's/\(.\)/\1 /g'  |
+	sed 's/ ; e w o r d/ ;eword /g' |
+	sed 's/\// ;esyll /g'|
+	tr -s ' ' > ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-tmp.tmp
+
+		mv ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-tmp.tmp ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-tags.txt
+
+	echo "creating gold versions"
+
+	sed 's/;esyll//g'  < ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-tags.txt |
+		tr -d ' ' |
+		sed 's/;eword/ /g' > ${RES_FOLDER}/processed/$LANGUAGE/${KEYNAME}-gold.txt
+
+done
+
+
+echo "end"
+
+##pcregrep --color='auto' -n '[^\x00-\x7F]' $PROCESSED_FILE2
--- recipes/acqDiv/README.txt
+++ recipes/acqDiv/README.txt
@@ -1,15 +1,44 @@
-Before any of the following, remember to mount the corpus to decrypt it:
+Once in your lifetime, you'll extract the relevant data and make a folder that will be transferred to oberon and encrypted. Inside this folder, please put three folders:
+- data: contains original .rda and extracted versions of the corpora, in language subfolders
+- processed: contains derived versions of the corpora (gold, tags, and temps), in language subfolders
+- results: will contain results, in language subfolders
 
-encfs ../../../.acqDivEncrypted ../../../acqDivVisible
+Once you're done, create a mounted folder for the encryption:
+encfs ABSOLUTEPATHS.acqDivEncrypted ABSOLUTEPATHS/acqDivVisible
+
+The system will ask whether you want to create the "source" and the "destination" folder (one at a time) -- say yes.
+
+Then transfer your files, like this:
+scp -r Documents/acqDivVisible/* USERNAME@129.199.81.30:ABSOLUTEPATHS/acqDivVisible
+
+IMPORTANT: you only need to do this once. 
+
+Thereafter, you just need to remember to mount the corpus to decrypt it:
+encfs ABSOLUTEPATHS.acqDivEncrypted ABSOLUTEPATHS/acqDivVisible
+
 
 and don’t forget to unmount it when you are done:
 
-fusermount -u ../../../acqDivVisible
+fusermount -u ABSOLUTEPATHS/acqDivVisible
 
-The R script used to extract utterances from the corpus doesn't need to be rerun each time, as, after all, those data will not ever change. Therefore, I'm not including it in the bigwrap script
 
 
 NOTES
-why use Z for sh?
-3 appeared as an onset and as a vowel
-please check my onset, vowel files
+- Why are there utterances that are empty or NA? We became aware of this when selecting for language=="Japanese", then the first 150 rows are NA
+
+for Japanese -- We are surprised about:
+- there are clusters (issho) -- are you not transcribing devoiced vowels?
+- There are words that are a single consonant ("b", "n") 
+- There are word-initial geminates (tte, kke)
+- How do we deal with allophony? Most s before i is transcribed sh; more rarely ʃ; and sometimes even left as s -- and sometimes sh before other Vs (Taishoo); both ts and t occur before i
+- What does ??? mean in for instance in the sequence:
+gueibe
+Kakka ???
+Kakka no
+Does it mean "not understood" or are we missing some characters?
+- is o: the same as oo?
+
+
+- Chintang: how do you syllabify diphthongs? What do accents on vowels mean? Should we remove the accent (i.e., they just mark stress) or should we use a different symbol for the vowel (i.e., à means a different quality from a)? What are symbols like this: aa -- a phonetically lengthened vowel (i.e., we should replace aa with a) or a phonologically long vowel (i.e., we should use a different symbol)?
+
+
